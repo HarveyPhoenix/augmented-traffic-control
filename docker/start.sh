284764fd13ce3db27f01423e578b45d805758a27
harveyhuang <harveyhuang@debianWorking>
2018-03-06 15:33:12 +0800

    add env ATCD_LISTEN_PORT

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,7 @@
-sed -i -e "s/{{ATCD_WAN}}/${ATCD_WAN}/
+sed -i -e "s/ATC_API = {/ATC_API = {'ATCD_PORT': ${ATCD_LISTEN_PORT},/" ./atcui/settings.py
+
+sed -i -e "s/{{ATCD_LISTEN_PORT}}/${ATCD_LISTEN_PORT}/
+    s/{{ATCD_WAN}}/${ATCD_WAN}/
     s/{{ATCD_LAN}}/${ATCD_LAN}/
     s/{{ATCD_MODE}}/${ATCD_MODE}/
     s/{{ATCD_OPTIONS}}/${ATCD_OPTIONS}/
